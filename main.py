ffbb393f0cef10cd313ad2fb48e2cbb1313853a3
Diego <luisdiegofv97@gmail.com>
2019-11-11 16:49:06 -0600

    Parte Grafica

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import math
+import pygame
+from random import randint
 
 # Analiza el estado del campo
 # Decide cuando rotar
@@ -33,12 +36,12 @@ Clausulas de Horn propuestas V2.0
 [0.0 : 0.2] | (1, 1)  | (-1, 1)  | (1, -1)   | (-10, -1)
 [0.2 : 0.7] | (5, 5)  | (-5, 5)  | (5, -5)   | (-10, -5)
 [0.7 : 1.0] | (10, 10)| (-10, 10)| (10, -10) | (-10, -10)
-Cambio en X y Y 
-''' 
+Cambio en X y Y
+'''
 
 '''
 Reglas
-IF x = [0.0 : 0.2] AND x, y = NO then m = (1, 1) -> moveto(x+1, y+1) 
+IF x = [0.0 : 0.2] AND x, y = NO then m = (1, 1) -> moveto(x+1, y+1)
 ..el resto..
 '''
 
@@ -61,7 +64,100 @@ orientation_ling = lambda x, y: 'NO' if (x == 1 and y == 1) else ('NE' if (x == 
 
 a = 6
 b = -7
-print(orientation_ling(sign(a), sign(b))) 
+print(orientation_ling(sign(a), sign(b)))
 
 # not used
 # orientation_ling = lambda a: 'N' if (a > 45 and a < 135) else ('O' if (a > 135 and a < 180) else ('E' if (a > 0 and a < 45) else 0))
+
+# pygame CONST
+WIDTH = 300  # width window
+HEIGHT = 600 # height window
+FPS = 30 # frames per second
+
+# pygames graphic display
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("SIMULACION")
+clock = pygame.time.Clock()
+
+# random positions
+# ball
+ball_x = WIDTH / 2 + randint(-140,140)
+ball_y = HEIGHT / 2 + randint(-250, 250)
+
+# player
+player_x = WIDTH / 2 + randint(-140,140)
+player_y = HEIGHT / 2 + randint(-250, 250)
+player_init_angle = randint(0, 360)
+
+# pygame classes
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('mario.png'))
+        self.imageOriginal = pygame.image.load(os.path.join('mario.png'))
+        self.rect = self.image.get_rect()
+        self.rect.center = (player_x, player_y)
+        self.rotation = 0
+        self.rotatePlayer(player_init_angle)
+
+    def update(self):
+        self.rect.x += 2
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+    def rotatePlayer(self, angle):
+        self.rotation += angle
+        self.image = pygame.transform.rotate(self.imageOriginal, self.rotation)
+        self.rect = self.image.get_rect()
+        self.rect.center = (ball_x, ball_y)
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((3, 3))
+        self.image.fill((255,100,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2 + (randint(-140,140)), HEIGHT / 2 + randint(-250, 250))
+
+# methods for calculations
+def ball_player_ang(bx = ball_x, by = ball_y, px = player_x, py = player_y):
+    triangle_x = bx - px
+    triangle_y = by - py
+    print(triangle_x)
+    print(triangle_y)
+    print(math.tanh(triangle_x / triangle_y))
+
+ball_player_ang()
+
+# sprites
+all_sprites = pygame.sprite.Group()
+player = Player()
+ball = Ball()
+all_sprites.add(player)
+all_sprites.add(ball)
+
+# display loop
+done = False
+while not done:
+    # FPS
+    clock.tick(FPS)
+
+    # key and display controls
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.rotatePlayer(15)
+
+    # update
+    screen.fill((0,0,0))
+
+    # render
+    all_sprites.draw(screen)
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(100, 0, 100, 2))
+    pygame.display.flip()
+
+# quit pygames
+pygame.quit()
